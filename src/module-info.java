89b999332dcd29df5884cdea7ce76d3907a9b259
Bris <dangngocquan104@gmail.com>
2022-04-18 20:16:19 +0700

    Create java project - branch master

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * Yah
+ * @author Bris
+ * @version 1.0
+ * @since 8:06:43 PM Apr 18, 2022
+ *
+ */
+module MIMGOMAT2317 {
+}
---
+++ src/week01/HelloWorld.java
@@ -0,0 +1,19 @@
+package week01;
+
+/**
+ * .
+ * @author Bris
+ * @version 1.0
+ * @since 8:09:20 PM -  Apr 18, 2022
+ */
+public class HelloWorld {
+	/**
+	 * The main method.
+	 * @param args Unused.
+	 */
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello world");
+		}
+	}
+}
