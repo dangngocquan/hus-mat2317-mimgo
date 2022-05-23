492d9f5e523ae7dd79c6d9b9c5e04eee94ab16ff
Bris <dangngocquan104@gmail.com>
2022-05-23 09:15:10 +0700

    .

---
+++ src/week12/Staff.java
@@ -0,0 +1,33 @@
+package week12;
+
+public class Staff extends Person {
+	private String school;
+	private double pay;
+	
+	public Staff(String name, String address, String school, double pay) {
+		super(name,address);
+		this.school = school;
+		this.pay = pay;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	public double getPay() {
+		return pay;
+	}
+
+	public void setPay(double pay) {
+		this.pay = pay;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Staff[%s,school=%s,pay=%s", super.toString(), this.school, this.pay);
+	}
+}
--- src/week12/Student.java
+++ src/week12/Student.java
@@ -7,5 +7,37 @@ public class Student extends Person {
 	
 	public Student(String name, String address, String program, int year, double fee) {
 		super(name, address);
+		this.program = program;
+		this.year = year;
+		this.fee = fee;
+	}
+
+	public String getProgram() {
+		return program;
+	}
+
+	public void setProgram(String program) {
+		this.program = program;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public double getFee() {
+		return fee;
+	}
+
+	public void setFee(double fee) {
+		this.fee = fee;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Student[%s,program=%s,year=%d,fee=%s]", super.toString(), this.program, this.year, this.fee);
 	}
 }
